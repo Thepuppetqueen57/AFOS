6225f3e7c61b8688165c1cb4c55daf102bf2d112
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-02-22 17:06:34 -0800

    Made the test program more advanced for testing
    Guys make AFOS in AFOS

--- programs/test.lua
+++ programs/test.lua
@@ -1,1 +1,9 @@
+function sleep(seconds)
+    local start_time = os.clock()
+
+    while os.clock() - start_time < seconds do end
+end
+
 print("hows it goin")
+sleep(0.5)
+print("welcome to the test program")
